e7fcb16b86ce0bf8693b7adb6460c1d6cc945101
Benjamin Shaya <bshaya@restdevices.com>
2015-04-22 22:32:46 +0000

    Added c program to toggle a pin on the RPI, potentially opening a door.

---
+++ bin/allow.c
@@ -0,0 +1,149 @@
+//Toggles an LED
+//uses sysfs
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define POUT 18
+
+#define HIGH_TIME 2000
+
+#define LOW 0
+#define HIGH 1
+
+#define IN 0
+#define OUT 1
+
+//enable pin for sysfs
+static int GPIOExport(int pin) {
+  #define BUFFER_MAX 35
+  char buffer[BUFFER_MAX];
+  ssize_t buffer_len;
+  int fd;
+
+  fd = open("/sys/class/gpio/export", O_WRONLY);
+  if (-1 == fd) {
+    fprintf(stderr, "Failed to open export for writing!\n");
+    return (-1);
+  }
+  buffer_len = snprintf(buffer, BUFFER_MAX, "%d", pin);
+  write(fd, buffer, buffer_len);
+  close(fd);
+  return(0);
+}
+
+//disable pin for sysfs
+static int GPIOUnexport(int pin) {
+  #define BUFFER_MAX 35
+  char buffer[BUFFER_MAX];
+  ssize_t buffer_len;
+  int fd;
+
+  fd = open("/sys/class/gpio/unexport", O_WRONLY);
+  if (-1 == fd) {
+    fprintf(stderr, "Failed to close export for writing!\n");
+    return (-1);
+  }
+  buffer_len = snprintf(buffer, BUFFER_MAX, "%d", pin);
+  write(fd, buffer, buffer_len);
+  close(fd);
+  return(0);
+}
+
+static int GPIODirection(int pin, int dir) {
+  
+  #define DIRECTION_MAX 35
+  char path[DIRECTION_MAX];
+  int fd;
+
+  snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
+  fd = open(path, O_WRONLY);
+  if (-1 == fd) {
+    fprintf(stderr, "Failed to open gpio direction for writing!\n");
+    return (-1);
+  }
+
+  int result;
+
+  if (dir == IN) {
+    result = write(fd, "in", strlen("in"));
+  } else {
+    result = write(fd, "out", strlen("out"));
+  }
+
+  if (result == -1) {
+    fprintf(stderr, "Failed to set Direction");
+    return(-1);
+  }
+  close(fd);
+  return(0);
+}
+
+static int GPIOWrite(int pin, int value) {
+  
+  #define VALUE_MAX 35
+  char path[VALUE_MAX];
+  int fd;
+
+  snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+  fd = open(path, O_WRONLY);
+  if (-1 == fd) {
+    fprintf(stderr, "Failed to open gpio value for writing!\n");
+    return (-1);
+  }
+
+  int result;
+
+  if (value == HIGH) {
+    result = write(fd, "1", strlen("1"));
+  } else {
+    result = write(fd, "0", strlen("0"));
+  }
+
+  if (result == -1) {
+    fprintf(stderr, "Failed to set value");
+    return(-1);
+  }
+  close(fd);
+  return(0);
+}
+
+int main(int argc, char *argv[]) {
+
+  int time = HIGH_TIME;
+
+  if (argc >= 2) {
+    time = atoi(argv[1]);
+  }
+
+  //export pin and set as output
+  if (-1 == GPIOExport(POUT)) {
+    return (1);
+  }
+
+  //delay to allow sysfs to open up gpio files
+  usleep(200 * 1000);
+
+  if (-1 == GPIODirection(POUT, OUT)) {
+    return (2);
+  }
+
+  if (-1 == GPIOWrite(POUT, HIGH)) {
+    return (3);
+  }
+  usleep(1000 * time);
+  if (-1 == GPIOWrite(POUT, LOW)) {
+    return (3);
+  }
+
+  if (-1 == GPIOUnexport(POUT)) {
+    return (4);
+  }
+  return (0);
+}
+
