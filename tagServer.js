e63b9180c0c2fb2ff6f1c8c88d76ee980b509354
Benjamin <bshaya@restdevices.com>
2015-04-23 16:00:16 -0400

    Clients send location, and groups.json links locations to group names.

--- tagServer.js
+++ tagServer.js
@@ -4,13 +4,15 @@ var https = require('https');
 var fs = require('fs');
 
 var users = require('./users.json');
-
 var config = require('./client_config.json');
+var groups = require('./groups.json');
 
+//Needed for self-signed root CA's
 require('ssl-root-cas')
   .inject()
   .addFile('./keys/private-root-ca.crt.pem');
 
+//Configure https
 var httpsOptions = {
   key: fs.readFileSync('./keys/server.key.pem'),
   cert: fs.readFileSync('./keys/server.crt.pem')
@@ -18,45 +20,43 @@ var httpsOptions = {
 
 var app = express();
 
-https.createServer(httpsOptions, app).listen(config.serverPort);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+//Main page just to check things are working
 app.get('/', function (req, res) {
   res.end("rfid server running")
 })
 
-app.get('/:group/check', function (req, res) {
+//access here!
+app.get('/:location/check', function (req, res) {
   console.log(req.body)
 
-  var group = req.params.group
+  var location = req.params.location
+  var group = groups[location]
 
   var rfid = req.body.rfid;
   if (!users.hasOwnProperty(rfid)) {
-    console.log("unrecognized tag: ",rfid)
+    console.log("unrecognized tag: %s attempted to access %s at %s",
+	       rfid, location, Date.now());
     return res.end(JSON.stringify({
       authorized: false
     }));
   }
   if (users[rfid].access[group]) {
-    console.log(users[rfid].name, " accessed ", group, "at ", Date.now())
+    console.log("%s accessed %s at %s",
+		users[rfid].name, location, Date.now());
     return res.end(JSON.stringify({
       authorized: true
     }));
   }
-  console.log("unauthorized attempt by", users[rfid].name, " to ", group,
-	      " at ", Date.now())
+  console.log("unauthorized attempt by %s to %s at %s",
+	      users[rfid].name, location, Date.now())
   return res.end(JSON.stringify({
     authorized: false
   }));
 });
 
-/*
-var server = app.listen(8080, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('RFID Server listening at http://%s:%s', host, port);
-})
-*/
+//start listening!
+https.createServer(httpsOptions, app).listen(config.serverPort);
+console.log('RFID Server Listening');
